05cdd42b0194223e4726f7162ea3be48ef0ec5fe
babiboo39 <ruli.kalit39@protonmail.com>
2020-07-12 06:28:50 +0700

    modify upload

--- admin/report/upload.php
+++ admin/report/upload.php
@@ -1,26 +1,9 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Upload Report</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-        <form enctype="multipart/form-data" action="#" method="post">
-            <input type="hidden" name="MAX_FILE_SIZE" value="100000">
-            Choose File to Upload:<br><br>
-            <input name="uploaded" type="file"><br>
-            <br>
-            <input type="submit" name="Upload" value="Upload">
-        </form>
-    </body>
-</html>
-
 <?php
+if (isset($_POST['submit'])) {
 
-if(isset ($_POST['Upload'])) {
-    $pathTarget = "files/";
-    $pathtarget = basename($_FILES['uploaded']['name']);
+    $target_dir  = 'files/';
+    $target_path =  $target_dir . basename($_FILES['uploaded']['name']);
+    $uploadOk    = false;
 
     if(!move_uploaded_file($_FILES['uploaded']['tmp_name'], $pathTarget)) {
         echo '<pre>Your file was not uploaded</pre>';
@@ -29,3 +12,20 @@ if(isset ($_POST['Upload'])) {
     }
 }
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
+    <body>
+        <form action="" method="post" enctype="multipart/form-data">
+            <p>Choose File:</p>
+            <input type="file" name="file" accept="*"/>
+            <br/>
+            <br/>
+            <input type="submit" name="submit"/>
+        </form>
+    </body>
+</html>
